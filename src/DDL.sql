e532b25e26fce46c46098a63af0f01205603c69e
Bootzy97 <98661433+Bootzy97@users.noreply.github.com>
2022-04-16 16:50:59 -0500

    Update DDL.sql
    the ACTUAL table codes for SQL database

--- src/DDL.sql
+++ src/DDL.sql
@@ -1,116 +1,147 @@
-create table classroom
-	(building		varchar(15),
-	 room_number		varchar(7),
-	 capacity		numeric(4,0),
-	 primary key (building, room_number)
-	);
+create table user_account (
+account_number 			numeric(5,0) not null,
+name 					varchar(30),
+primary key (account_number)
+);
 
-create table department
-	(dept_name		varchar(20), 
-	 building		varchar(15), 
-	 budget		        numeric(12,2) check (budget > 0),
-	 primary key (dept_name)
-	);
+create table Login_Information (
+username 				varchar(30),
+password 				varchar(30),
+account_number 			numeric(5,0) not null,
+primary key (username),
+foreign key (account_number) references user_account(account_number)
+);
 
-create table course
-	(course_id		varchar(8), 
-	 title			varchar(50), 
-	 dept_name		varchar(20),
-	 credits		numeric(2,0) check (credits > 0),
-	 primary key (course_id),
-	 foreign key (dept_name) references department (dept_name)
-		on delete set null
-	);
+create table Address (
+address_id 				numeric(5,0),
+state 					varchar(30),
+city 					varchar(30),
+street 					varchar(50),
+zip_code 				numeric(5,0),
+account_number 			numeric(5,0) not null,
+primary key (address_id),
+foreign key (account_number) references user_account(account_number) on delete cascade
+);
 
-create table instructor
-	(ID			varchar(5), 
-	 name			varchar(20) not null, 
-	 dept_name		varchar(20), 
-	 salary			numeric(8,2) check (salary > 29000),
-	 primary key (ID),
-	 foreign key (dept_name) references department (dept_name)
-		on delete set null
-	);
+create table Payment_Method (
+payment_id 				numeric (5,0),
+name 					varchar(30),
+type 					varchar(20),
+information 			varchar(50),
+account_number 			numeric(5,0) not null,
+primary key (payment_id),
+foreign key (account_number) references user_account(account_number) on delete cascade
+);
 
-create table section
-	(course_id		varchar(8), 
-         sec_id			varchar(8),
-	 semester		varchar(6)
-		check (semester in ('Fall', 'Winter', 'Spring', 'Summer')), 
-	 year			numeric(4,0) check (year > 1701 and year < 2100), 
-	 building		varchar(15),
-	 room_number		varchar(7),
-	 time_slot_id		varchar(4),
-	 primary key (course_id, sec_id, semester, year),
-	 foreign key (course_id) references course (course_id)
-		on delete cascade,
-	 foreign key (building, room_number) references classroom (building, room_number)
-		on delete set null
-	);
+create table Product (
+  Product_ID			varchar(8),
+  name 					varchar(30),
+  Quantity				numeric(10,0),
+  Shelf_ID				varchar(8),
+  primary key (product_ID)
+);
 
-create table teaches
-	(ID			varchar(5), 
-	 course_id		varchar(8),
-	 sec_id			varchar(8), 
-	 semester		varchar(6),
-	 year			numeric(4,0),
-	 primary key (ID, course_id, sec_id, semester, year),
-	 foreign key (course_id, sec_id, semester, year) references section (course_id, sec_id, semester, year)
-		on delete cascade,
-	 foreign key (ID) references instructor (ID)
+create table customer_order (
+order_number 			numeric(5,0) not null,
+Product_ID				varchar(8),
+order_date 				varchar(30),
+account_number 			numeric(5,0) not null,
+primary key (order_number),
+foreign key (account_number) references user_account(account_number) on delete cascade,
+foreign key (product_ID) references product(product_ID) on delete cascade
+);
+
+create table delivery_method (
+  delivery_id 			numeric(5,0),
+  type 					varchar(30),
+  order_number 			numeric(5,0) not null,
+  primary key (delivery_id),
+  foreign key (order_number) references customer_order(order_number) on delete cascade
+);
+
+create table order_history(
+  history_id 			numeric(5,0),
+  account_number 		numeric(5,0) not null,
+  order_number 			numeric(5,0) not null,
+  primary key(history_id),
+  foreign key (account_number) references user_account(account_number) on delete cascade,
+  foreign key (order_number) references customer_order(order_number)
+);
+
+create table Warehouse
+    (Warehouse_num		numeric(25,0),
+    Product_ID			varchar(8),
+    Quantity			numeric(10,0),
+	primary key (Warehouse_num),
+    foreign key (Product_ID) references Product (Product_ID)
+    	on delete cascade
+    );
+
+	create table Supplier
+    (Supplier_ID		varchar(8),
+    Name				varchar(20) not null,
+    Product_ID			varchar(8),
+    primary key (Supplier_ID),
+    foreign key (Product_ID) references Warehouse (Product_ID)
+    	on delete cascade
+    );
+
+    create table truck_driver
+	(Truck_ID		varchar(8),
+    Name			varchar(20) not null,
+    Product_ID		varchar(8),
+    quantity		numeric(10,0),
+    primary key (Truck_ID),
+    foreign key (Product_ID) references Supplier (Product_ID)
 		on delete cascade
-	);
+    );
 
-create table student
-	(ID			varchar(5), 
-	 name			varchar(20) not null, 
-	 dept_name		varchar(20), 
-	 tot_cred		numeric(3,0) check (tot_cred >= 0),
-	 primary key (ID),
-	 foreign key (dept_name) references department (dept_name)
-		on delete set null
-	);
+    create table Store
+    (Store_ID			varchar(8),
+    Location			varchar(50),
+    primary key (Store_ID)
+    );
 
-create table takes
-	(ID			varchar(5), 
-	 course_id		varchar(8),
-	 sec_id			varchar(8), 
-	 semester		varchar(6),
-	 year			numeric(4,0),
-	 grade		        varchar(2),
-	 primary key (ID, course_id, sec_id, semester, year),
-	 foreign key (course_id, sec_id, semester, year) references section (course_id, sec_id, semester, year)
-		on delete cascade,
-	 foreign key (ID) references student (ID)
-		on delete cascade
-	);
+    create table Aisle
+    (Aisle_num			numeric(2,0),
+    Catagory			varchar(20),
+    shelf_num			numeric(4,0),
+    primary key (Aisle_num)
+    );
 
-create table advisor
-	(s_ID			varchar(5),
-	 i_ID			varchar(5),
-	 primary key (s_ID),
-	 foreign key (i_ID) references instructor (ID)
-		on delete set null,
-	 foreign key (s_ID) references student (ID)
-		on delete cascade
-	);
+    create table shelf
+    (Shelf_ID			varchar(8),
+    Catagory			Varchar(20),
+    Product_ID			varchar(8),
+    primary key (shelf_ID),
+    foreign key (product_ID) references product (Product_ID)
+    );
 
-create table time_slot
-	(time_slot_id		varchar(4),
-	 day			varchar(1),
-	 start_hr		numeric(2) check (start_hr >= 0 and start_hr < 24),
-	 start_min		numeric(2) check (start_min >= 0 and start_min < 60),
-	 end_hr			numeric(2) check (end_hr >= 0 and end_hr < 24),
-	 end_min		numeric(2) check (end_min >= 0 and end_min < 60),
-	 primary key (time_slot_id, day, start_hr, start_min)
-	);
+    create table Manager
+    (Manager_ID 		varchar(8),
+    Name				varchar(20) not null,
+    PhoneNumber			varchar(12),
+    primary key (Manager_ID)
+    );
 
-create table prereq
-	(course_id		varchar(8), 
-	 prereq_id		varchar(8),
-	 primary key (course_id, prereq_id),
-	 foreign key (course_id) references course (course_id)
-		on delete cascade,
-	 foreign key (prereq_id) references course (course_id)
-	);
+    create table Employee
+    (Employee_ID		varchar(8),
+    Name				varchar(20) not null,
+    PhoneNumber			varchar(12),
+    primary key (Employee_ID)
+    );
+
+    create table Delivery_Driver
+    (Driver_ID			varchar(8),
+    Name				varchar(20) not null,
+    PhoneNumber			varchar(12),
+    primary key (Driver_ID)
+    );
+
+    create table Company
+    (Company_ID			varchar(8),
+    Name				varchar(20) not null,
+    Location			varchar(50),
+    primary key (Company_ID)
+    );
 
