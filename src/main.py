04bc44108c0556070dfb4c651b8d73ded2c34997
sfrembling <sfrembling@gmail.com>
2022-04-16 13:01:27 -0500

    added DDL base & basic GUI

--- src/main.py
+++ src/main.py
@@ -2,7 +2,11 @@ import mysql.connector as sql # for database
 import tkinter as tk          # for GUI
 
 def main():
-    pass
+    root = tk.Tk() # create a window
+    root.title("MySQL Database") # set window title
+    root.geometry("500x500") # set window size
+    root.resizable(False, False) # disable window resizing
+    root.mainloop() # start the GUI
 
 if __name__ == '__main__':
     main()
