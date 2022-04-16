2791730a3b55e83714bd3a7d62815f3887b06686
WesleyGlover <WesleyGlover@my.unt.edu>
2022-04-16 15:54:16 -0500

    Merging DDL files

--- src/DDL.sql
+++ src/DDL.sql
@@ -1,11 +1,11 @@
-create table login_information (
+create table Login_Information (
 username varchar(30),
 password varchar(30),
 primary key (username),
 foreign key (account_number) references account(account_number)
 )
 
-create table account (
+create table Account (
 account_number numeric not null auto_increment,
 name varchar(30),
 primary key (account_number),
@@ -13,32 +13,32 @@ foreign key (username) references login_information(username),
 foreign key (order_number) references order(order_number)
 )
 
-create table address (
+create table Address (
 state varchar(30),
 city varchar(30),
 street varchar(50),
-zip_code numeric,
+zip_code numeric(5,0),
 primary key (account_number) references account(account_number) on delete cascade
 )
 
-create table payment_method (
+create table Payment_Method (
 name varchar(30),
 type varchar(20),
 information varchar(50),
 primary key (account_number) references account(account_number) on delete cascade
 )
 
-create table product (
-  product_ID numeric not null,
+create table Product (
+  product_ID numeric(5,0) not null,
   name varchar(30),
-  quantity numeric,
+  quantity numeric(10,0),
   primary key (product_ID),
-  foreign key (shelf_number) references shelf(shelf_number)
+  foreign key (shelf_ID) references shelf(shelf_ID)
 )
 
 create table order (
 order_number numeric not null auto_increment,
-product_ID numeric not null,
+product_ID numeric(5,0) not null,
 order_date varchar(30),
 primary key (order_number),
 foreign key (account_number) references account(account_number) on delete cascade,
@@ -54,3 +54,80 @@ create table order_history(
   primary key (account_number) references account(account_number) on delete cascade,
   foreign key (order_number) references order(order_number)
 )
+
+create table Warehouse
+    (Warehouse_num		numeric(25,0),
+    Product_ID			varchar(8),
+    Quantity			numeric(10,0),
+	primary key (Warehouse_num),
+    foreign key (Product_ID, Quantity) references Product (Product_ID, Quantity)
+    	on delete cascade
+    );
+
+	create table Supplier
+    (Supplier_ID		varchar(8),
+    Name				varchar(20) not null,
+    Product_ID			varchar(8),
+    primary key (Supplier_ID),
+    foreign key (Product_ID) references Warehouse (Product_ID)
+    	on delete cascade
+    );
+
+    create table truck_driver
+	(Truck_ID		varchar(8),
+    Name			varchar(20) not null,
+    Product_ID		varchar(8),
+    quantity		numeric(10,0),
+    primary key (Truck_ID),
+    foreign key (Product_ID) references Supplier (Product_ID)
+		on delete cascade
+    );
+
+    create table Store
+    (Store_ID			varchar(8),
+    Location			varchar(50),
+    primary key (Store_ID)
+    );
+
+    create table Aisle
+    (Aisle_num			numeric(2,0),
+    Catagory			varchar(20),
+    shelf_num			numeric(4,0),
+    primary key (Aisle_num)
+    );
+
+    create table shelf
+    (Shelf_ID			varchar(8),
+    Catagory			Varchar(20),
+    Product_ID			varchar(8),
+    primary key (shelf_ID),
+    foreign key (product_ID) references product (Product_ID)
+    );
+
+    create table Manager
+    (Manager_ID 		varchar(8),
+    Name				varchar(20) not null,
+    PhoneNumber			varchar(12),
+    primary key (Manager_ID)
+    );
+
+    create table Employee
+    (Employee_ID		varchar(8),
+    Name				varchar(20) not null,
+    PhoneNumber			varchar(12),
+    primary key (Employee_ID)
+    );
+
+    create table Delivery_Driver
+    (Driver_ID			varchar(8),
+    Name				varchar(20) not null,
+    PhoneNumber			varchar(12),
+    primary key (Driver_ID)
+    );
+
+    create table Company
+    (Company_ID			varchar(8),
+    Name				varchar(20) not null,
+    Location			varchar(50),
+    primary key (Company_ID)
+    );
